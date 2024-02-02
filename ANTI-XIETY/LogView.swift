b4bccde9d9bac820de6666dc1786d86521673520
takeirabridges <takeirabridges@gmail.com>
2024-02-02 13:21:00 -0500

    Updated Code

---
+++ ANTI-XIETY/LogView.swift
@@ -0,0 +1,18 @@
+//
+//  LogView.swift
+//  ANTI-XIETY
+//
+//  Created by Baye Hanne on 2/1/24.
+//
+
+import SwiftUI
+
+struct LogView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LogView()
+}
